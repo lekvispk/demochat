cefc8a9300d7de4f5cbe2add9d8ed2f9a227d502
Elvis Campos <lekvispk@gmail.com>
2016-11-24 23:44:10 -0500

    test10

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,29 @@ class MessagesController < ApplicationController
   respond_to :html, :js
   
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+  #For all responses in this controller, return the CORS access control headers.
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:9292'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = 'http://localhost:9292'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = 'http://localhost:9292'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = 'http://localhost:9292'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+
   # GET /messages
   # GET /messages.json
   def index
